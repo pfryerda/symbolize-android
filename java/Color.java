700d028e466d42421d3a3c4d1b29087171d92396
luke-michael-brown <luke.michael.brown@gmail.com>
2014-05-02 15:39:00 -0400

    Added Color.java

---
+++ java/Color.java
@@ -0,0 +1,5 @@
+public enum Color {
+	BLACK, WHITE, RED, ORANGE, YELLOW, GREEN, BLUE, CYAN, PURPLE
+}
+
+// This module will become obsolete after merging into android studio (androids studio contains their own)
