13468acfccd2354c60ed137c730ba632cb1e55ff
Luke-Michael-Brown <Luke.Michael.Brown@gmail.com>
2014-06-21 14:10:07 -0400

    Added more error checking to xml parser
    Xml parser is mostly safe, could use more error checking in preamble +
    make more generic

--- Symbolize/app/src/main/java/symbolize/app/Common/PuzzleDB.java
+++ Symbolize/app/src/main/java/symbolize/app/Common/PuzzleDB.java
@@ -14,6 +14,9 @@ import java.util.LinkedList;
 
 import symbolize.app.R;
 
+/*
+ * Expcetion class for xml parsing specific exceptions
+ */
 class InvalidXmlException extends Exception {
     // Constructors
     //--------------
@@ -27,6 +30,10 @@ class InvalidXmlException extends Exception {
     }
 }
 
+
+/*
+ * Class used to parse stoed xml files and load them into memory
+ */
 public class PuzzleDB {
     // Static fields
     //---------------
@@ -52,7 +59,7 @@ public class PuzzleDB {
      * Method used to get level from xml resource files
      */
     public Level fetch_level( int worldNum, int levelNum ) {
-
+        // Set up level variables
         String hint = "";
         int drawRestirction = 0;
         int eraseRestirction = 0;
@@ -62,58 +69,145 @@ public class PuzzleDB {
         ArrayList<LinkedList<Line>> boards = new ArrayList<LinkedList<Line>>( new LinkedList<LinkedList<Line>>() );
         ArrayList<LinkedList<Line>> solutions =  new ArrayList<LinkedList<Line>>( new LinkedList<LinkedList<Line>>() );
 
+        int puzzle_id = id_offset + ( NUMBEROFLEVELSPERWORLD ) * ( worldNum - 1 ) + ( levelNum - 1 );
+        XmlResourceParser xpp = res.getXml( puzzle_id );
+
         try {
-            int puzzle_id = id_offset + ( NUMBEROFLEVELSPERWORLD ) * ( worldNum - 1 ) + ( levelNum - 1 );
-            XmlResourceParser xpp = res.getXml( puzzle_id );
-            xpp.next();
+            // Set up temp variables
             ArrayList<LinkedList<Line>> tmpArray =  null;
             LinkedList<Line> tmpList = null;
             Line tmpLine = null;
             Posn tmpP1 = null;
             Posn tmpP2 = null;
-            int tmpColor = -1;
+            Integer tmpColor = null;
+            Integer tmpX = null;
+            Integer tmpY = null;
 
+            // Manually parse preamble
+            xpp.next();
+            xpp.next();
+            xpp.next();
+
+            if ( !xpp.getName().equals( "hint" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, xpp.getName() );
+            }
+            xpp.next();
+            hint = xpp.getText();
+            xpp.next();
+            if ( !xpp.getName().equals( "hint" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, "/"+xpp.getName() );
+            }
+
+            xpp.next();
+
+            if ( !xpp.getName().equals( "drawRestirction" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, xpp.getName() );
+            }
+            xpp.next();
+            drawRestirction = Integer.parseInt( xpp.getText().trim() );
+            xpp.next();
+            if ( !xpp.getName().equals( "drawRestirction" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, "/"+xpp.getName() );
+            }
+
+            xpp.next();
+
+            if ( !xpp.getName().equals( "eraseRestirction" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, xpp.getName() );
+            }
+            xpp.next();
+            eraseRestirction = Integer.parseInt( xpp.getText().trim() );
+            xpp.next();
+            if ( !xpp.getName().equals( "eraseRestirction" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, "/"+xpp.getName() );
+            }
+
+            xpp.next();
+
+            if ( !xpp.getName().equals( "rotateEnabled" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, xpp.getName() );
+            }
+            xpp.next();
+            rotateEnabled = Boolean.valueOf( xpp.getText() );
+            xpp.next();
+            if ( !xpp.getName().equals( "rotateEnabled" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, "/"+xpp.getName() );
+            }
+
+            xpp.next();
+
+            if ( !xpp.getName().equals( "flipEnabled" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, xpp.getName() );
+            }
+            xpp.next();
+            flipEnabled = Boolean.valueOf( xpp.getText() );
+            xpp.next();
+            if ( !xpp.getName().equals( "flipEnabled" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, "/"+xpp.getName() );
+            }
+
+            xpp.next();
+
+            if ( !xpp.getName().equals( "colourEnabled" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, xpp.getName() );
+            }
+            xpp.next();
+            colourEnabled = Boolean.valueOf( xpp.getText() );
+            xpp.next();
+            if ( !xpp.getName().equals( "colourEnabled" ) ) {
+                throw new InvalidXmlException( worldNum, levelNum, "/"+xpp.getName() );
+            }
+
+            // Parse boarsd and solutions
             for ( int eventType = xpp.getEventType(); eventType != XmlResourceParser.END_DOCUMENT; eventType = xpp.next() ){
+
                 switch ( eventType ) {
 
                     case ( XmlPullParser.START_TAG ): {
                         String topTag = xpp.getName();
-                        if ( topTag.equals( "graph" ) || topTag.equals( "Level" ) || topTag.equals( "Line" ) || topTag.equals( "boards" ) || topTag.equals( "solutions" ) ) {
-                            if ( topTag.equals( "graph" ) ) {
+                        if ( topTag.equals( "Level" ) || topTag.equals( "boards" ) || topTag.equals( "solutions" ) || topTag.equals( "graph" ) ||  topTag.equals( "Line" ) ||  topTag.equals( "p1" ) || topTag.equals( "p2" )  ) {
+                            if ( topTag.equals( "boards" ) || topTag.equals( "solutions" ) ) {
+                                if ( tmpArray != null || tmpList != null || tmpLine != null || tmpP1 != null || tmpP2 != null || tmpColor != null || tmpX != null || tmpY != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
+                                }
+                                tmpArray = new ArrayList<LinkedList<Line>>();
+                            } else if ( topTag.equals( "graph" ) ) {
+                                if ( tmpArray == null || tmpList != null || tmpLine != null || tmpP1 != null || tmpP2 != null || tmpColor != null || tmpX != null || tmpY != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
+                                }
                                 tmpList = new LinkedList<Line>();
                             } else if ( topTag.equals( "Line" ) ) {
+                                if ( tmpArray == null || tmpList == null || tmpLine != null || tmpP1 != null || tmpP2 != null || tmpColor != null || tmpX != null || tmpY != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
+                                }
                                 tmpLine = new Line();
-                            } else if ( topTag.equals( "boards" ) || topTag.equals( "solutions" ) ) {
-                                tmpArray = new ArrayList<LinkedList<Line>>();
+                            } else if ( topTag.equals( "p1" ) ) {
+                                if ( tmpArray == null || tmpList == null || tmpLine == null || tmpP1 != null || tmpX != null || tmpY != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
+                                }
+                                tmpP1 = new Posn();
+                            } else if ( topTag.equals( "p2" ) ) {
+                                if ( tmpArray == null || tmpList == null || tmpLine == null || tmpP2 != null || tmpX != null || tmpY != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
+                                }
+                                tmpP2 = new Posn();
                             }
                         } else {
                             xpp.next();
-                            if ( topTag.equals( "hint" ) ) {
-                                hint = xpp.getText();
-                            } else if ( topTag.equals( "drawRestirction" ) ) {
-                                drawRestirction = Integer.parseInt( xpp.getText().trim() );
-                            } else if ( topTag.equals( "eraseRestirction" ) ) {
-                                eraseRestirction = Integer.parseInt( xpp.getText().trim() );
-                            } else if ( topTag.equals( "rotateEnabled" ) ) {
-                                rotateEnabled = Boolean.valueOf( xpp.getText() );
-                            } else if ( topTag.equals( "flipEnabled" ) ) {
-                                flipEnabled = Boolean.valueOf( xpp.getText() );
-                            } else if ( topTag.equals( "colourEnabled" ) ) {
-                                colourEnabled = Boolean.valueOf( xpp.getText() );
-                            } else if ( topTag.equals( "p1" ) || topTag.equals( "p2" ) ) {
-                                xpp.next();
-                                int first = Integer.parseInt( xpp.getText().trim() );
-                                xpp.next();
-                                xpp.next();
-                                xpp.next();
-                                int second = Integer.parseInt( xpp.getText().trim() );
-                                xpp.next();
-                                if (topTag.equals( "p1" ) ) {
-                                    tmpP1 = new Posn( first, second );
-                                } else {
-                                    tmpP2 = new Posn( first, second );
+                            if ( topTag.equals( "x" ) ) {
+                                if ( tmpArray == null || tmpList == null || tmpLine == null || ( tmpP1 == null && tmpP2 == null ) || tmpX != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
                                 }
+                                tmpX = Integer.valueOf( xpp.getText().trim() );
+                            } else if ( topTag.equals( "y" ) ) {
+                                if ( tmpArray == null || tmpList == null || tmpLine == null || ( tmpP1 == null && tmpP2 == null ) || tmpY != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
+                                }
+                                tmpY = Integer.valueOf( xpp.getText().trim() );
                             } else if ( topTag.equals( "color" ) ) {
+                                if ( tmpArray == null || tmpList == null || tmpLine == null || tmpColor != null || tmpX != null || tmpY != null ) {
+                                    throw new InvalidXmlException( worldNum, levelNum, topTag );
+                                }
                                 tmpColor = Integer.valueOf( xpp.getText() );
                             } else {
                                 throw new InvalidXmlException(worldNum, levelNum, topTag);
@@ -122,7 +216,7 @@ public class PuzzleDB {
 
                             String bottomTag = xpp.getName();
                             if (!topTag.equals(bottomTag)) {
-                                throw new InvalidXmlException(worldNum, levelNum, topTag, bottomTag);
+                                throw new InvalidXmlException(worldNum, levelNum, topTag, "/"+bottomTag);
                             }
                         }
                         break;
@@ -141,13 +235,27 @@ public class PuzzleDB {
                             }
                             solutions = tmpArray;
                         } else if ( xpp.getName().equals( "graph" ) ) {
-                            if ( tmpList == null ) {
-                                throw new InvalidXmlException( worldNum, levelNum, "/graph" );
+                            if (tmpList == null) {
+                                throw new InvalidXmlException(worldNum, levelNum, "/graph");
                             }
-                            tmpArray.add( ( LinkedList<Line> ) tmpList.clone() );
+                            tmpArray.add( ( LinkedList<Line> ) tmpList.clone());
                             tmpList = null;
+                        } else if ( xpp.getName().equals( "p1" ) ) {
+                            if ( tmpP1 == null || tmpX == null || tmpY == null ) {
+                                throw new InvalidXmlException(worldNum, levelNum, "/Posn");
+                            }
+                            tmpP1 = new Posn( tmpX, tmpY );
+                            tmpX = null;
+                            tmpY = null;
+                        } else if ( xpp.getName().equals( "p2" ) ) {
+                            if ( tmpP2 == null || tmpX == null || tmpY == null ) {
+                                throw new InvalidXmlException(worldNum, levelNum, "/Posn");
+                            }
+                            tmpP2 = new Posn( tmpX, tmpY );
+                            tmpX = null;
+                            tmpY = null;
                         } else if ( xpp.getName().equals( "Line" ) ) {
-                            if ( tmpLine == null || tmpP1 == null || tmpP2 == null || tmpColor == -1 ) {
+                            if ( tmpLine == null || tmpP1 == null || tmpP2 == null || tmpColor == null ) {
                                 throw new InvalidXmlException( worldNum, levelNum, "/Line" );
                             }
                             tmpLine = new Line( tmpP1, tmpP2, tmpColor );
@@ -155,7 +263,7 @@ public class PuzzleDB {
                             tmpLine = null;
                             tmpP1 = null;
                             tmpP2 = null;
-                            tmpColor = -1;
+                            tmpColor = null;
                         }
                         break;
                     }
