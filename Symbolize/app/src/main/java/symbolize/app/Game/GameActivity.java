a11638c6191a83c46a8b30897b581daeb49c4d54
Luke-Michael-Brown <Luke.Michael.Brown@gmail.com>
2014-07-06 12:48:48 -0400

    Fixed ads. Ad support now working!

--- Symbolize/app/src/main/java/symbolize/app/Game/GameActivity.java
+++ Symbolize/app/src/main/java/symbolize/app/Game/GameActivity.java
@@ -37,7 +37,6 @@ public class GameActivity extends Activity  {
     public static final boolean DEVMODE = false;
     public static final int SCALING = 1000;
     public static Point SCREENSIZE;
-    public static int BARHEIGHT;
     public static final String LUKE = "Awesome";
 
 
@@ -68,8 +67,8 @@ public class GameActivity extends Activity  {
     @Override
     protected void onCreate( final Bundle savedInstanceState ) {
         // Basic setup
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_game );
 
         // Variable setup
         puzzleDB = new PuzzleDB( getResources() );
@@ -80,7 +79,7 @@ public class GameActivity extends Activity  {
         AdView adView = ( AdView ) this.findViewById( R.id.game_adspace );
         AdRequest ad_request = new AdRequest.Builder()
                 .addTestDevice( AdRequest.DEVICE_ID_EMULATOR )
-                .addTestDevice( Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID) )
+                .addTestDevice( Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID ) ) // TOD: Manually put in our device ids for security
                 .build();
         adView.setAdListener(new AdListener() {} );
         adView.loadAd(ad_request);
@@ -90,25 +89,20 @@ public class GameActivity extends Activity  {
         final Display DISPLAY = getWindowManager().getDefaultDisplay();
         SCREENSIZE = new Point();
         DISPLAY.getSize( SCREENSIZE );
-
-        LinearLayout foreground = (LinearLayout) findViewById(R.id.canvas);
-        foreground.getLayoutParams().height = SCREENSIZE.x;
-        foreground.getLayoutParams().width = SCREENSIZE.x;
+        int size = ( SCREENSIZE.y > SCREENSIZE.x ) ? SCREENSIZE.x : SCREENSIZE.y;
 
         LinearLayout background = (LinearLayout) findViewById(R.id.background);
-        background.getLayoutParams().height = SCREENSIZE.x;
-        background.getLayoutParams().width = SCREENSIZE.x;
+        background.getLayoutParams().height = size;
+        background.getLayoutParams().width = size;
 
-        BARHEIGHT = ( SCREENSIZE.y - SCREENSIZE.x - AdSize.BANNER.getHeight() ) / 2;
-        findViewById( R.id.topbar ).getLayoutParams().height = BARHEIGHT;
-        findViewById( R.id.buttons ).getLayoutParams().height = BARHEIGHT;
+        LinearLayout foreground = (LinearLayout) findViewById(R.id.canvas);
+        foreground.getLayoutParams().height = size;
+        foreground.getLayoutParams().width = size;
 
         Bitmap bitMap_fg = Bitmap.createScaledBitmap(
-                Bitmap.createBitmap( SCREENSIZE.x, SCREENSIZE.x, Bitmap.Config.ARGB_8888 ),
-                SCALING, SCALING, true );
+                Bitmap.createBitmap( size, size, Bitmap.Config.ARGB_8888 ), SCALING, SCALING, true );
         Bitmap bitMap_bg = Bitmap.createScaledBitmap(
-                Bitmap.createBitmap( SCREENSIZE.x, SCREENSIZE.x, Bitmap.Config.ARGB_8888 ),
-                SCALING, SCALING, true );
+                Bitmap.createBitmap( size, size, Bitmap.Config.ARGB_8888 ), SCALING, SCALING, true );
 
 
         // Set up Game
