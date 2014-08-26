50c0d76127dc8d90602522a5d2e852f9dd00ce2a
Luke-Michael-Brown <Luke.Michael.Brown@gmail.com>
2014-08-25 21:13:52 -0400

    Added dynamic animation times

--- Symbolize/app/src/main/java/symbolize/app/DataAccess/MetaDataAccess.java
+++ Symbolize/app/src/main/java/symbolize/app/DataAccess/MetaDataAccess.java
@@ -12,6 +12,23 @@ abstract public class MetaDataAccess {
     // Static fields
     //--------------
 
+    public static final byte ANIMATION_DECREASE_THRESHOLD = 1;
+
+    public static final short ROTATE_DURATION_MAX = 500;
+    public static final short ROTATE_DURATION_MIN = 340;
+
+    public static final short FLIP_DURATION_MAX = 500;
+    public static final short FLIP_DURATION_MIN = 340;
+
+    public static final short SHIFT_DURATION_MAX = 626;
+    public static final short SHIFT_DURATION_MIN = 464;
+
+    public static final short ZOOM_DURATION_MAX = 646;
+    public static final short ZOOM_DURATION_MIN = 484;
+
+    public static final short TRANSLATE_DURATION_MAX = 774;
+    public static final short TRANSLATE_DURATION_MIN = 616;
+
     private static DataAccessObject dao = new DataAccessObject( R.string.preference_meta_key );
 
 
@@ -60,6 +77,26 @@ abstract public class MetaDataAccess {
 
     public static boolean Has_seen_special() {
         return dao.Get_property( Page.Get_resource_string( R.string.seen_special ), false ) || Session.DEV_MODE;
+    }
+
+    public static short Get_rotate_duration() {
+        return get_duration( R.string.rotate_duration, ROTATE_DURATION_MIN, ROTATE_DURATION_MAX );
+    }
+
+    public static short Get_flip_duration() {
+        return get_duration( R.string.flip_duration, FLIP_DURATION_MIN, FLIP_DURATION_MAX );
+    }
+
+    public static short Get_shift_duration() {
+        return get_duration( R.string.options_show_animations, SHIFT_DURATION_MIN, SHIFT_DURATION_MAX );
+    }
+
+    public static short Get_zoom_duration() {
+        return get_duration( R.string.zoom_duration, ZOOM_DURATION_MIN, ZOOM_DURATION_MAX );
+    }
+
+    public static short Get_translate_duration() {
+        return get_duration( R.string.translate_duration, TRANSLATE_DURATION_MIN, TRANSLATE_DURATION_MAX );
     }
 
 
@@ -133,14 +170,45 @@ abstract public class MetaDataAccess {
         dao.Set_property( Page.Get_context().getString( R.string.last_world ), 1 );
         dao.Set_property( Page.Get_context().getString( R.string.last_draw ), true );
 
-        dao.Set_property( Page.Get_resource_string(R.string.seen_world ), false);
-        dao.Set_property(Page.Get_resource_string(R.string.seen_draw ), false);
+        dao.Set_property( Page.Get_resource_string( R.string.seen_world ), false);
+        dao.Set_property(Page.Get_resource_string( R.string.seen_draw ), false);
         dao.Set_property( Page.Get_resource_string( R.string.seen_rotate ), false );
-        dao.Set_property( Page.Get_resource_string(R.string.seen_erase  ), false );
-        dao.Set_property( Page.Get_resource_string(R.string.seen_flip ), false);
-        dao.Set_property( Page.Get_resource_string(R.string.seen_shift ), false);
-        dao.Set_property( Page.Get_resource_string(R.string.seen_drag ), false );
+        dao.Set_property( Page.Get_resource_string( R.string.seen_erase  ), false );
+        dao.Set_property( Page.Get_resource_string( R.string.seen_flip ), false);
+        dao.Set_property( Page.Get_resource_string( R.string.seen_shift ), false);
+        dao.Set_property( Page.Get_resource_string( R.string.seen_drag ), false );
         dao.Set_property( Page.Get_resource_string( R.string.seen_color_change ), false );
         dao.Set_property( Page.Get_resource_string( R.string.seen_special ), false );
+
+        dao.Set_property( Page.Get_resource_string( R.string.rotate_duration ), ROTATE_DURATION_MAX );
+        dao.Set_property( Page.Get_resource_string( R.string.flip_duration ), FLIP_DURATION_MAX );
+        dao.Set_property( Page.Get_resource_string( R.string.shift_duration ), SHIFT_DURATION_MAX );
+        dao.Set_property( Page.Get_resource_string( R.string.zoom_duration ), ZOOM_DURATION_MAX );
+        dao.Set_property( Page.Get_resource_string( R.string.translate_duration ), TRANSLATE_DURATION_MAX );
+    }
+
+
+    // Private methods
+    //-----------------
+
+    /*
+     * A helper method for get animation durations, gets the duration and push the new duration for you
+     *
+     * @param int id: The id for the dao
+     * @param short duration_min: The shortest the animation can be
+     * @param short duration_max: The default value for the animation
+     *
+     * @return short: The current duration
+     */
+    private static short get_duration( int id, short duration_min, short duration_max ) {
+        if ( Session.DEV_MODE ) {
+            return duration_min;
+        }
+
+        short duration = (short) dao.Get_property( Page.Get_resource_string( id ), duration_max );
+        if ( duration > duration_min ) {
+            dao.Set_property( Page.Get_resource_string( id ), duration - ANIMATION_DECREASE_THRESHOLD );
+        }
+        return duration;
     }
 }
